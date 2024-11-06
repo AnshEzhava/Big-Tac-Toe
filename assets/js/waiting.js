2a0078ec01aebf65a2e7ca944e927a373b0f43da
Ansh Ezhava <anshdpsg@gmail.com>
2024-11-06 20:59:40 +0530

    b

--- assets/js/waiting.js
+++ assets/js/waiting.js
@@ -1,6 +1,6 @@
 const urlParams = new URLSearchParams(window.location.search);
 const sessionId = urlParams.get('sessionId');
-const gameLink = `${window.location.origin}/join.html?sessionId=${sessionId}`;
+const gameLink = `${window.location.origin}/Big-Tac-Toe/join.html?sessionId=${sessionId}`;
 
 document.getElementById('session-link').textContent = gameLink;
 
