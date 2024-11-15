73f4d971e82b3e8f265bf2860454dfbf076ad7a6
Ansh Ezhava <anshdpsg@gmail.com>
2024-11-15 21:32:33 +0530

    backend down

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,34 +1,41 @@
 const button = document.getElementById("online-btn");
-button.addEventListener("click", createGame);
 
-function createGame() {
-  fetch(
-    "https://bigtactoe-backend.azurewebsites.net/api/games/create",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ status: "OPEN" }),
-    }
-  )
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error("Failed to create game session");
-      }
-    })
-    .then((data) => {
-      const sessionId = data.id;
-      if (sessionId) {
-        window.location.href = `waiting.html?sessionId=${sessionId}`;
-      } else {
-        console.error("Session ID not found in response");
-      }
-    })
-    .catch((error) => console.error("Error creating game: ", error));
+button.addEventListener('mouseover', function() {
+    button.textContent = "Backend Down";
+});
 
-  //bigtactoe-backend-production.up.railway.app
-  //localhost:8080
-}
+button.addEventListener('mouseout', function(){
+    button.textContent = "Online Mode";
+});
+
+// function createGame() {
+//   fetch(
+//     "https://bigtactoe-backend.azurewebsites.net/api/games/create",
+//     {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({ status: "OPEN" }),
+//     }
+//   )
+//     .then((response) => {
+//       if (response.ok) {
+//         return response.json();
+//       } else {
+//         throw new Error("Failed to create game session");
+//       }
+//     })
+//     .then((data) => {
+//       const sessionId = data.id;
+//       if (sessionId) {
+//         window.location.href = `waiting.html?sessionId=${sessionId}`;
+//       } else {
+//         console.error("Session ID not found in response");
+//       }
+//     })
+//     .catch((error) => console.error("Error creating game: ", error));
+
+//   //bigtactoe-backend-production.up.railway.app
+//   //localhost:8080
+// }
